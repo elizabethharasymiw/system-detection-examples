c522123746165c4a495aa634369ed0bedde18fcd
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-12 16:21:08 -0500

    Print pointers directly instead of casting

--- stack-growth-direction-example/main.c
+++ stack-growth-direction-example/main.c
@@ -3,25 +3,27 @@
 
 /*
  * @brief Function to determine if the stack grows up or down
+ * @apiNote This has issue with the optimizer potentailly not declaring
+ *          variables in the order we thought they were.
  * @return boolean true if the stack grew down and false if it grew up
  */
 bool stackGrewDown(){
   volatile int x = 0;
   volatile int y = 0;
-  long long xAddress = (long long)&x;
-  long long yAddress = (long long)&y;
 
-  printf("x: %lld\n", xAddress);
-  printf("y: %lld\n", yAddress);
+  printf("x: %p\n", &x);
+  printf("y: %p\n", &y);
 
-  if (xAddress > yAddress){
-    return false;
+  if (&x > &y){
+    return true;
   }
-  return true;
+  return false;
 }
 
 /*
  * @brief Function to determine if the stack grows up or down
+ * @apiNote Avoids the issue with the optimizer in the first version of
+            this function.
  * @param other Takes an int pointer which will be used to see the
  *        address of a variable between two stack frames
  * @return boolean true if the stack grew down and false if it grew up
@@ -32,8 +34,8 @@ bool stackGrewDownTwo(int* other){
   if(!other){
     return stackGrewDownTwo(&x);
   }else{
-   printf("x1: %lld\n", (long long) other);
-   printf("x2: %lld\n", (long long) &x);
+   printf("x1: %p\n", other);
+   printf("x2: %p\n", &x);
    return &x > other;
   }
 }
@@ -41,9 +43,9 @@ bool stackGrewDownTwo(int* other){
 int main(){
   printf("\n");
   printf("Version 1:\n");
-  printf("The stack grew %s\n", stackGrewDown() ? "down" : "up");
+  printf("The stack grew %s\n", stackGrewDown() ? "up" : "down");
   printf("\n");
   printf("Version 2:\n");
-  printf("The stack grew %s\n", stackGrewDownTwo(NULL) ? "down" : "up");
+  printf("The stack grew %s\n", stackGrewDownTwo(NULL) ? "up" : "down");
   return 0;
 }
