4ce6cb3f4b241cf6b2849c9d5c9602a2b61ef985
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-12 10:37:42 -0500

    Add recursive solution to finding what direction the stack grows

--- stack-growth-direction-example/main.c
+++ stack-growth-direction-example/main.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 
 /*
- * Function to determine if the stack grows up or down
- * Returns true if the stack grew down and false if it grew up.
+ * @brief Function to determine if the stack grows up or down
+ * @return boolean true if the stack grew down and false if it grew up
  */
 bool stackGrewDown(){
   volatile int x = 0;
@@ -20,7 +20,28 @@ bool stackGrewDown(){
   return true;
 }
 
+/*
+ * @brief Function to determine if the stack grows up or down
+ * @param other Takes an int pointer which will be used to see the
+ *        address of a variable between two stack frames
+ * @return boolean true if the stack grew down and false if it grew up
+ */
+bool stackGrewDownTwo(int* other){
+  int x;
+
+  if(!other){
+    return stackGrewDownTwo(&x);
+  }else{
+   return &x > other;
+  }
+}
+
 int main(){
+  printf("\n");
+  printf("Version 1:\n");
   printf("The stack grew %s\n", stackGrewDown() ? "down" : "up");
+  printf("\n");
+  printf("Version 2:\n");
+  printf("The stack grew %s\n", stackGrewDownTwo(NULL) ? "down" : "up");
   return 0;
 }
