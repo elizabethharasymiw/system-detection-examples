0dd9f6bfc124d18068162a4621bc4468ff8c0cb9
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-07-19 20:35:58 -0500

    Add code for determining which way the heap grows

--- heap-growth-direction-example/main.c
+++ heap-growth-direction-example/main.c
@@ -24,19 +24,20 @@ void printHeapExample(){
   printf("y0: %p\n", &y[1]);
 }
 
-// TODO:
-//bool heapGrewDown(){
-//  int* x = (int*)malloc(sizeof(int) * 10);
-//  int* y = (int*)malloc(sizeof(int) * 10);
-//}
+bool heapGrewDown(){
+
+  int* x = (int*)malloc(sizeof(int) * 2);
+  printf("x0: %p\n", &x[0]);
+  printf("x1: %p\n", &x[1]);
+
+  if(&x[0] < &x[1]){
+    return true;
+  }
+
+  return false;
+}
 
 int main(){
-
-  printHeapExample();
-
-  //TODO:
-  //printf("\n");
-  //printf("The heap grew %s\n", heapGrewDown() ? "up" : "down");
-
+  printf("The heap grew %s\n", heapGrewDown() ? "up" : "down");
   return 0;
 }
