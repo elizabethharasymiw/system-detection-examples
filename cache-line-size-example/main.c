8a0996f546f0f5c963808927fcf39831ae63d492
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-07-20 22:46:31 -0500

    Add new TODO example for getting the size of the cache line

---
+++ cache-line-size-example/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// TODO
+//int getCacheLineSize(){
+//  return 0;
+//}
+
+int main(){
+  printf("Hello, World!");
+  //printf("The size of the current cache line is: %d", getCacheLineSize());
+  return 0;
+}
