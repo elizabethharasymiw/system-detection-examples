5d7c2db4bb46f10a5a9d145287faed9e25975271
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-07-21 22:50:47 -0500

    Add function to create a random char between A-Z

--- cache-line-size-example/main.c
+++ cache-line-size-example/main.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * Generate a random capitalized letter
+ * A-Z equal 65-90 in ASCII code
+ */
+char randomChar(){
+  int  myRandomInt = (rand() % 25) + 65;
+  char myRandomChar = (char)myRandomInt;
+  return myRandomChar;
+}
 
 // TODO
 //int getCacheLineSize(){
@@ -6,7 +18,16 @@
 //}
 
 int main(){
-  printf("Hello, World!");
+
+  srand(time(0)); // Seed random number generator with current time
+
+  printf("Hello, World!\n");
+
+  for(int i = 0; i < 100; i++){
+    printf("%c", randomChar());
+  }
+
   //printf("The size of the current cache line is: %d", getCacheLineSize());
+
   return 0;
 }
