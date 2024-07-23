8b8c33bc3389b38f3ece0be38525ca6b10965f3f
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-07-23 01:40:00 -0500

    Add C example of using cpuid to get cacheline size

---
+++ cache-line-size-examples/c-x86-cpuid-cache-line-size-example/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cpuid.h>
+
+int main() {
+    // Declare four unsigned integer variables to store CPU information
+    unsigned int eax, ebx, ecx, edx;
+
+    // The __cpuid intrinsic fetches the CPU information based on the value in the eax register.
+    // The first argument (1) specifies the information type (Processor Info and Feature Bits).
+    // The results are stored in the variables eax, ebx, ecx, and edx.
+    __cpuid(1, eax, ebx, ecx, edx);
+
+    // Extract the cache line size from the ebx register.
+    // The cache line size is stored in bits 15:8 of the ebx register.
+    // The mask 0xFF00 isolates these bits, and the right shift by 5 aligns the value correctly.
+    unsigned int cacheLineSize = (ebx & 0xFF00) >> 5;
+
+    printf("Cache line size: %u bytes\n", cacheLineSize);
+
+    return 0;
+}
+
