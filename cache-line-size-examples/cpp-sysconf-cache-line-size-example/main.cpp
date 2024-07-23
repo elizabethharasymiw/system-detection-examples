c22ad720bd17177265fb840ec1ed35cdb1edba25
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-07-22 23:56:50 -0500

    Add cache size and different cache level examples to sysconf example

--- cache-line-size-examples/cpp-sysconf-cache-line-size-example/main.cpp
+++ cache-line-size-examples/cpp-sysconf-cache-line-size-example/main.cpp
@@ -7,11 +7,27 @@
 #include <unistd.h>
 
 int main() {
-    long cacheLineSize = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
-    if (cacheLineSize == -1) {
-        std::cerr << "sysconf failed to get cache line size" << std::endl;
-        return 1;
-    }
-    std::cout << "Cache line size: " << cacheLineSize << " bytes" << std::endl;
+    std::cout << "Level 1 Cache:" << std::endl;
+    std::cout << "Instruction Cache Size: " << sysconf(_SC_LEVEL1_ICACHE_SIZE) << " bytes" << std::endl;
+    std::cout << "Instruction Cache Line Size: " << sysconf(_SC_LEVEL1_ICACHE_LINESIZE) << " bytes" << std::endl;
+    std::cout << "Instruction Cache Associativity: " << sysconf(_SC_LEVEL1_ICACHE_ASSOC) << " bytes" << std::endl;
+    std::cout << "Data Cache Size: " << sysconf(_SC_LEVEL1_DCACHE_SIZE) << " bytes" << std::endl;
+    std::cout << "Data Cache Line Size: " << sysconf(_SC_LEVEL1_DCACHE_LINESIZE) << " bytes" << std::endl;
+    std::cout << "Data Cache Associativity: " << sysconf(_SC_LEVEL1_DCACHE_ASSOC) << " bytes" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Level 2 Cache:" << std::endl;
+    std::cout << "Cache Size: " << sysconf(_SC_LEVEL2_CACHE_SIZE) << " bytes" << std::endl;
+    std::cout << "Cache Line Size: " << sysconf(_SC_LEVEL2_CACHE_LINESIZE) << " bytes" << std::endl;
+    std::cout << "Cache Associativity: " << sysconf(_SC_LEVEL2_CACHE_ASSOC) << " bytes" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Level 3 Cache:" << std::endl;
+    std::cout << "Cache Size: " << sysconf(_SC_LEVEL3_CACHE_SIZE) << " bytes" << std::endl;
+    std::cout << "Cache Line Size: " << sysconf(_SC_LEVEL3_CACHE_LINESIZE) << " bytes" << std::endl;
+    std::cout << "Cache Associativity: " << sysconf(_SC_LEVEL3_CACHE_ASSOC) << " bytes" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Level 4 Cache:" << std::endl;
+    std::cout << "Cache Size: " << sysconf(_SC_LEVEL4_CACHE_SIZE) << " bytes" << std::endl;
+    std::cout << "Cache Line Size: " << sysconf(_SC_LEVEL4_CACHE_LINESIZE) << " bytes" << std::endl;
+    std::cout << "Cache Associativity: " << sysconf(_SC_LEVEL4_CACHE_ASSOC) << " bytes" << std::endl;
     return 0;
 }
